602a5c65c609636d8e8439398330b26bdbf00297
SzymonFit <szymek.fit@gmail.com>
2024-05-13 22:19:18 +0200

    GUI v2

--- MRP.py
+++ MRP.py
@@ -101,4 +101,4 @@ if __name__ == "__main__":
     example_prod = pd.Series(data=[0,0,0,28,0,30])
     mrp1 = MRP(22, 3, 40, example_prod)
     mrp1.calculate_MRP()
-    # print(mrp1)
+    
--- main.py
+++ main.py
@@ -4,30 +4,86 @@ import json
 import pandas as pd
 from MRP import MRP
 from pandastable import Table, TableModel
+pd.set_option('future.no_silent_downcasting', True)
 
 def jsonWindow(data, filename):
-    # Create a new window to display JSON data
+
     newWindow = tk.Toplevel()
     newWindow.title("JSON Data")
-    
-    # Create a text widget to display JSON data
     text_widget = tk.Text(newWindow, width=100, height=30)
     text_widget.pack()
-
-    # Insert the JSON data into the text widget
     text_widget.insert(tk.END, json.dumps(data, indent=4))
-
-    # Save changes to JSON file
     with open(filename, 'w') as file:
         json.dump(data, file, indent=4)
 
-def calculateZelazoMRP(bom, item):
+def calcAllMRPs():
     mrp_window = tk.Toplevel(root)
-    mrp_window.title("MRP Table")
+    mrp_window.title("MRP Tables")
 
-    na_stanie = bom[item]['params']['available']
-    czas_realizacji = bom[item]['params']['lead_time']
-    wielkosc_partii = bom[item]['params']['batch_size']
+    for i, item in enumerate(bom['hantla_do_cwiczen']):
+        
+        frame = ttk.Frame(mrp_window)
+        frame.grid(row=i, column=0, sticky="nsew")  
+        label_item = tk.Label(frame, text=f"{item} MRP Table", font=("Helvetica", 12, "bold"))
+        label_item.grid(row=2, column=0, columnspan=2)  
+
+        na_stanie = bom['hantla_do_cwiczen'][item]['params']['available']
+        czas_realizacji = bom['hantla_do_cwiczen'][item]['params']['lead_time']
+        wielkosc_partii = bom['hantla_do_cwiczen'][item]['params']['batch_size']
+        calk_zap = pd.Series(data=[0,0,0,28,0,30])
+
+        elMRP = MRP(na_stanie, czas_realizacji, wielkosc_partii, calk_zap)
+        mrpTable = elMRP.calculate_MRP()
+
+        row_names = ["calkowite zapotrzebowanie", "planowane przyjecia", "przewidywane na stanie", "zapotrzebowanie netto", "planowane zamowienia", "planowane przyjecie zamowien"]
+        mrpTable.index = pd.Index(row_names)
+
+        table_model = TableModel(dataframe=mrpTable)
+        dataframe_table = Table(frame, model=table_model)
+        dataframe_table.show()
+
+        label_table = tk.Label(frame, text=f"{item.capitalize()} Table", font=("Helvetica", 10))
+        label_table.grid(row=0, column=0, columnspan=2, sticky="w")
+        
+
+        ttk.Separator(mrp_window, orient='horizontal').grid(row=i+1, column=0, columnspan=2, sticky="ew")
+
+    # for i, item in enumerate(bom['hantla_do_cwiczen']['ciezarki']):
+    #     if item == 'params':
+    #         pass
+    #     frame = ttk.Frame(mrp_window)
+    #     frame.grid(row=i, column=0, sticky="nsew")  
+    #     label_item = tk.Label(frame, text=f"{item} MRP Table", font=("Helvetica", 12, "bold"))
+    #     label_item.grid(row=2, column=0, columnspan=2)  
+
+    #     na_stanie = bom['hantla_do_cwiczen']['ciezarki']['params']['available']
+    #     czas_realizacji = bom['hantla_do_cwiczen']['ciezarki']['params']['lead_time']
+    #     wielkosc_partii = bom['hantla_do_cwiczen']['ciezarki']['params']['batch_size']
+    #     calk_zap = pd.Series(data=[0,0,0,28,0,30])
+
+    #     elMRP = MRP(na_stanie, czas_realizacji, wielkosc_partii, calk_zap)
+    #     mrpTable = elMRP.calculate_MRP()
+
+    #     row_names = ["calkowite zapotrzebowanie", "planowane przyjecia", "przewidywane na stanie", "zapotrzebowanie netto", "planowane zamowienia", "planowane przyjecie zamowien"]
+    #     mrpTable.index = pd.Index(row_names)
+
+    #     table_model = TableModel(dataframe=mrpTable)
+    #     dataframe_table = Table(frame, model=table_model)
+    #     dataframe_table.show()
+
+    #     label_table = tk.Label(frame, text=f"{item.capitalize()} Table", font=("Helvetica", 10))
+    #     label_table.grid(row=0, column=0, columnspan=2, sticky="w")
+        
+
+    #     ttk.Separator(mrp_window, orient='horizontal').grid(row=i+1, column=0, columnspan=2, sticky="ew")
+
+def calculateMRP(bom, item):
+    mrp_window = tk.Toplevel(root)
+    mrp_window.title(f"MRP Table for {item.capitalize()}")
+
+    na_stanie = bom['hantla_do_cwiczen'][item]['params']['available']
+    czas_realizacji = bom['hantla_do_cwiczen'][item]['params']['lead_time']
+    wielkosc_partii = bom['hantla_do_cwiczen'][item]['params']['batch_size']
     calk_zap = pd.Series(data=[0,0,0,28,0,30])
 
     zelazoMRP = MRP(na_stanie, czas_realizacji, wielkosc_partii, calk_zap)
@@ -36,8 +92,6 @@ def calculateZelazoMRP(bom, item):
 
     row_names = ["calkowite zapotrzebowanie", "planowane przyjecia", "przewidywane na stanie", "zapotrzebowanie netto", "planowane zamowienia", "planowane przyjecie zamowien"]
     mrpTable.index = pd.Index(row_names)
-    print(mrpTable.index)
-
 
     mrpTable.columns.name = "Tygodnie"
     
@@ -47,50 +101,47 @@ def calculateZelazoMRP(bom, item):
     dataframe_table.show()
 
 def updateParameters(entries, bom, item, filename):
-    bom[item]["params"]["quantity"] = entries[0].get()
-    bom[item]["params"]["lead_time"] = entries[1].get()
-    bom[item]["params"]["batch_size"] = entries[2].get()
-    bom[item]["params"]["available"] = entries[3].get()
+    bom["hantla_do_cwiczen"][item]["params"]["quantity"] = int(entries[0].get())
+    bom["hantla_do_cwiczen"][item]["params"]["lead_time"] = int(entries[1].get())
+    bom["hantla_do_cwiczen"][item]["params"]["batch_size"] = int(entries[2].get())
+    bom["hantla_do_cwiczen"][item]["params"]["available"] = int(entries[3].get())
 
     with open(filename, 'w') as f:
-        json.dump(bom, f)
+        json.dump(bom, f, indent=4)
 
 def updateParametersOfSubitem(entries, bom, item, subitem, filename):
-    bom[item][subitem]["params"]["quantity"] = entries[0].get()
-    bom[item][subitem]["params"]["lead_time"] = entries[1].get()
-    bom[item][subitem]["params"]["batch_size"] = entries[2].get()
-    bom[item][subitem]["params"]["available"] = entries[3].get()
+    bom["hantla_do_cwiczen"][item][subitem]["params"]["quantity"] = int(entries[0].get())
+    bom["hantla_do_cwiczen"][item][subitem]["params"]["lead_time"] = int(entries[1].get())
+    bom["hantla_do_cwiczen"][item][subitem]["params"]["batch_size"] = int(entries[2].get())
+    bom["hantla_do_cwiczen"][item][subitem]["params"]["available"] = int(entries[3].get())
 
     with open(filename, 'w') as f:
-        json.dump(bom, f)
-    
-    # jsonWindow(bom,filename)
+        json.dump(bom, f, indent=4)
 
 def createTab(notebook, bom, item, filename):
     frame = ttk.Frame(notebook)
-
     label_quantity = tk.Label(frame, text=f"{item.capitalize()} Quantity:", font=("Helvetica", 12))
     label_quantity.pack()
     entry_quantity = tk.Entry(frame, font=("Helvetica", 12), width=10)
-    entry_quantity.insert(0, bom[item]["params"]["quantity"])
+    entry_quantity.insert(0, bom["hantla_do_cwiczen"][item]["params"]["quantity"])
     entry_quantity.pack(pady=5)
 
     label_lead_time = tk.Label(frame, text=f"Lead Time [in days]: ", font=("Helvetica", 10))
     label_lead_time.pack()
     entry_lead_time = tk.Entry(frame, font=("Helvetica", 12), width=10)
-    entry_lead_time.insert(0, bom[item]["params"]["lead_time"])
+    entry_lead_time.insert(0, bom["hantla_do_cwiczen"][item]["params"]["lead_time"])
     entry_lead_time.pack(pady=5)
 
     label_batch_size = tk.Label(frame, text=f"Batch Size: ", font=("Helvetica", 10))
     label_batch_size.pack()
     entry_batch_size = tk.Entry(frame, font=("Helvetica", 12), width=10)
-    entry_batch_size.insert(0, bom[item]["params"]["batch_size"])
+    entry_batch_size.insert(0, bom["hantla_do_cwiczen"][item]["params"]["batch_size"])
     entry_batch_size.pack(pady=5)
 
     label_available = tk.Label(frame, text=f"Available: ", font=("Helvetica", 10))
     label_available.pack()
     entry_available = tk.Entry(frame, font=("Helvetica", 12), width=10)
-    entry_available.insert(0, bom[item]["params"]["available"])
+    entry_available.insert(0, bom["hantla_do_cwiczen"][item]["params"]["available"])
     entry_available.pack(pady=5)
 
     entries = [entry_quantity, entry_lead_time, entry_batch_size, entry_available]
@@ -98,16 +149,17 @@ def createTab(notebook, bom, item, filename):
     button_update = tk.Button(frame, text="Update", font=("Helvetica", 12), command=lambda: updateParameters(entries, bom, item, filename))
     button_update.pack(pady=5)
 
-    
+    button_mrp = tk.Button(root, text=f"Show MRP for {item.capitalize()}", font=("Helvetica", 12), command=lambda: calculateMRP(bom, item))
+    button_mrp.pack(pady=5)
 
-    if item == "ciezarki" and isinstance(bom[item], dict):
+    if item == "ciezarki" and isinstance(bom["hantla_do_cwiczen"][item], dict):
         sub_frame = ttk.Frame(frame)
         sub_frame.pack(pady=10, padx=10)
 
         subnotebook = ttk.Notebook(sub_frame)
         subnotebook.pack(fill='both', expand=True)
 
-        for sub_item in bom[item]:
+        for sub_item in bom["hantla_do_cwiczen"][item]:
             if sub_item != "params":
                 sub_tab = ttk.Frame(subnotebook)
                 sub_tab.pack(fill='both', expand=True)
@@ -115,25 +167,25 @@ def createTab(notebook, bom, item, filename):
                 sub_label_quantity = tk.Label(sub_tab, text=f"{sub_item.capitalize()} Quantity:", font=("Helvetica", 12))
                 sub_label_quantity.pack()
                 sub_entry_quantity = tk.Entry(sub_tab, font=("Helvetica", 12), width=10)
-                sub_entry_quantity.insert(0, bom[item][sub_item]["params"]["quantity"])
+                sub_entry_quantity.insert(0, bom["hantla_do_cwiczen"][item][sub_item]["params"]["quantity"])
                 sub_entry_quantity.pack(pady=5)
 
                 sub_label_lead_time = tk.Label(sub_tab, text=f"Lead Time [in days]:", font=("Helvetica", 10))
                 sub_label_lead_time.pack()
                 sub_entry_lead_time = tk.Entry(sub_tab, font=("Helvetica", 12), width=10)
-                sub_entry_lead_time.insert(0, bom[item][sub_item]["params"]["lead_time"])
+                sub_entry_lead_time.insert(0, bom["hantla_do_cwiczen"][item][sub_item]["params"]["lead_time"])
                 sub_entry_lead_time.pack(pady=5)
 
                 sub_label_batch_size = tk.Label(sub_tab, text=f"Batch Size:", font=("Helvetica", 10))
                 sub_label_batch_size.pack()
                 sub_entry_batch_size = tk.Entry(sub_tab, font=("Helvetica", 12), width=10)
-                sub_entry_batch_size.insert(0, bom[item][sub_item]["params"]["batch_size"])
+                sub_entry_batch_size.insert(0, bom["hantla_do_cwiczen"][item][sub_item]["params"]["batch_size"])
                 sub_entry_batch_size.pack(pady=5)
 
                 sub_label_available = tk.Label(sub_tab, text=f"Available:", font=("Helvetica", 10))
                 sub_label_available.pack()
                 sub_entry_available = tk.Entry(sub_tab, font=("Helvetica", 12), width=10)
-                sub_entry_available.insert(0, bom[item][sub_item]["params"]["available"])
+                sub_entry_available.insert(0, bom["hantla_do_cwiczen"][item][sub_item]["params"]["available"])
                 sub_entry_available.pack(pady=5)
 
                 sub_entries = [sub_entry_quantity, sub_entry_lead_time, sub_entry_batch_size, sub_entry_available]
@@ -147,7 +199,6 @@ def createTab(notebook, bom, item, filename):
     else:
         notebook.add(frame, text=item.capitalize())
 
-
 with open('hantla.json', 'r') as file:
     bom = json.load(file)
 
@@ -160,9 +211,9 @@ notebook.pack(pady=20, padx=20)
 filename = "hantla.json"
 
 for item in bom['hantla_do_cwiczen']:
-    createTab(notebook, bom['hantla_do_cwiczen'], item, filename)
+    createTab(notebook, bom, item, filename)
 
-button_mrp = tk.Button(root, text="Show MRP", font=("Helvetica", 12), command=lambda: calculateZelazoMRP(bom, item))
+button_mrp = tk.Button(root, text=f"Show all MRPs", font=("Helvetica", 12), command=lambda: calcAllMRPs())
 button_mrp.pack(pady=5)
 
 show_json = tk.Button(root, text="Show JSON Data", font=("Helvetica", 12), command=lambda: jsonWindow(bom, filename))
